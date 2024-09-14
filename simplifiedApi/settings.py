6bf918d68686d8ae2d2bbae117e2d7c30347b57a
shynishyni <168768099+shynishyni@users.noreply.github.com>
2024-09-14 16:55:07 +0530

    Update settings.py

--- simplifiedApi/settings.py
+++ simplifiedApi/settings.py
@@ -96,7 +96,7 @@ DATABASES = {
         'NAME': os.getenv('DB_NAME'),
         'USER': os.getenv('DB_USER'),
         'PASSWORD': os.getenv('DB_PASSWORD'),
-        'HOST': simplified-api.vercel.app,
+        'HOST': os.getenv('DB_HOST'),
         'PORT': os.getenv('DB_PORT', '3306'),
     }
 }
